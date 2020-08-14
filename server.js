dd022aa7aae47d54fec743286cea9024364ace93
HarshitaAgarwal09 <harshitaagarwal98@yahoo.com>
2020-08-15 01:14:52 +0530

    .

--- server.js
+++ server.js
@@ -7,17 +7,6 @@ const { resolve } = require('path');
 
 const app = express();
 
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
-
-
 const db = config.get('mongoURI');
 
 app.use(express.json());
@@ -36,7 +25,16 @@ mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTo
     .then(() => console.log("Connected to Database"))
     .catch((err) => console.log(err));
 
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
-});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
