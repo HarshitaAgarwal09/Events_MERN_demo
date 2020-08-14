3161849a5311dc84cba25e3ae4f1a74e7cb4c852
HarshitaAgarwal09 <harshitaagarwal98@yahoo.com>
2020-08-15 00:30:59 +0530

    .

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const config = require('config');
+
+const path = require('path');
+const { resolve } = require('path');
 
 const app = express();
 
-const config = require('config');
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path,resolve(__dirname,'client','build','index.html'));
+    })
+}
+
 
 const db = config.get('mongoURI');
 
@@ -14,7 +26,6 @@ app.use((req, res, next) => {
     next();
   });
   
-
 //Routes
 app.use('/api/events', require('./routes/api/events'));
 app.use('/api/auth', require('./routes/api/auth'));
@@ -25,7 +36,6 @@ mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTo
     .catch((err) => console.log(err));
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
 });
